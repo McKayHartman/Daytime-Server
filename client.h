2fed37aa23786b0b7afad5a3d7989e9b0bfaf879
Logan Hunt <l.b.hunt5@gmail.com>
2025-09-21 16:57:09 -0700

    bug fixes

--- client.h
+++ client.h
@@ -1,19 +1,19 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
 
 // socket bind, listen, accept
 #include <arpa/inet.h>
-#include <sys/socket.h>
 #include <netinet/in.h>
+#include <sys/socket.h>
 
 // DNS lookup
 #include <netdb.h>
 
 // read/write/close
-#include <sys/uio.h>
 #include <sys/types.h>
+#include <sys/uio.h>
 #include <unistd.h>
 
 #include <signal.h>
@@ -21,7 +21,6 @@
 #define DAYTIME_HOST "time.nst.gov"
 #define DAYTIME_PORT "13"
 
-
 #define MAX_MESSAGE_LENGTH 80
 
-void read_time(int client_socket);
+void get_time(int client_socket);
