3e68a7f3dca50ab995063a5a48112cfabb6f8a84
Lacy Hamilton <145398607+lacyhamilton@users.noreply.github.com>
2025-10-05 17:59:43 -0700

    Create client.h

---
+++ 3.5UDP/client.h
@@ -0,0 +1,21 @@
+// preprocessor directives
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <stdio.h>          // printf
+#include <stdlib.h>         // exit
+#include <strings.h>
+#include <sys/types.h>
+#include <arpa/inet.h>      // inet_addr, htons, htonl, inet_pton, sockaddr_in
+#include <sys/socket.h>     // socket, connect, send, recv
+#include <netinet/in.h>
+#include <unistd.h>         // close
+
+
+#define DAYTIME_HOST "time.nst.gov"
+#define PORT 13
+
+#define MAX_MSG_LEN 80
+
+
+#endif // CLIENT_H
